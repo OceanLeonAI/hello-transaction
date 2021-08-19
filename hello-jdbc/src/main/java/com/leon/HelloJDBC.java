3f49a52c1087f8ea597dae715d69067b4b916222
OceanLeonAI <beyondjayfir@126.com>
2021-08-19 12:58:46 +0800

    新增 jdbc 相关模块

---
+++ hello-jdbc/src/main/java/com/leon/HelloJDBC.java
@@ -0,0 +1,28 @@
+package com.leon;
+
+/**
+ * @PROJECT_NAME: hello-transaction
+ * @CLASS_NAME: HelloJDBC
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2021/8/19 12:56
+ * @Version 1.0
+ * @DESCRIPTION:
+ **/
+public class HelloJDBC {
+
+    public static void main(String[] args) {
+        helloJDBC();
+    }
+
+    /**
+     * JDBC 入门
+     */
+    private static void helloJDBC() {
+        // 注册驱动
+
+        // 获取连接
+
+        // 执行查询
+
+    }
+}
